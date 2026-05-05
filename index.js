a36709f56d177575bb1982ed8e7c541d937bc510
Vladimir Agafonkin <agafonkin@gmail.com>
2026-05-05 23:24:03 +0300

    slightly modernize & clean up

--- index.js
+++ index.js
@@ -4,45 +4,44 @@ const ESCAPE_RE = /[&<>'"]/g;
 const BREAK_RE = /^(\r\n|\r|\n)/;
 const W_LEFT_RE = /^[ \t]+(\r\n|\r|\n)/;
 const W_RIGHT_RE = /[ \t]+$/;
+const TPL_ESC_RE = /[\\`\r]|\$\{/g;
 const INCLUDE_RE = /include\(\s*(['"])(.+?)\1\s*(,\s*({.+?})\s*)?\)/g;
+
+const tplEscapes = {'\\': '\\\\', '`': '\\`', '\r': '\\r', '${': '\\${'};
+const escapeTpl = c => tplEscapes[c];
+
+const xmlEscapes = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '\'': '&apos;', '"': '&quot;'};
+const escapeChar = c => xmlEscapes[c];
+
+const AsyncFunction = (async () => {}).constructor;
 
 const defaultOptions = {
     escape: escapeXML,
     localsName: 'locals',
-    resolve: (parent, path) => path
+    resolve: (_, path) => path
 };
 
-let AsyncFunction;
-
 export function compile(ejs, options) {
-    options = Object.assign({cache: {}}, defaultOptions, options);
+    options = {cache: {}, ...defaultOptions, ...options};
     const {escape, localsName, context, filename, async} = options;
 
     const code = `'use strict'; ${compilePart(ejs, filename, options)}`;
-
-    if (async && !AsyncFunction) {
-        try {
-            AsyncFunction = (new Function('return (async () => {}).constructor'))();
-        } catch {
-            throw new Error('This environment does not support async/await.');
-        }
-    }
     const fn = new (async ? AsyncFunction : Function)(localsName, '_esc', '_str', code);
 
-    return data => fn.call(context, data || null, escape, stringify);
+    return data => fn.call(context, data ?? null, escape, stringify);
 }
 
 function compilePart(ejs, filename, options) {
     const {locals, localsName} = options;
-    let code = locals && locals.length ? `const {${locals.join(', ')}} = ${localsName}; ` : '';
+    let code = locals?.length ? `const {${locals.join(', ')}} = ${localsName}; ` : '';
     code += 'let _out = `';
 
-    const originalLastIndex = RE.lastIndex;
-    let lastIndex = RE.lastIndex = 0;
+    const tokens = ejs.matchAll(RE);
+    let lastIndex = 0;
     let match, prev, open;
 
     do {
-        match = RE.exec(ejs);
+        match = tokens.next().value;
         const token = match && match[0];
 
         if (prev !== '<%#') {
@@ -52,7 +51,7 @@ function compilePart(ejs, filename, options) {
                 if (prev === '_%>') str = str.replace(W_LEFT_RE, '');
                 else if (prev === '-%>') str = str.replace(BREAK_RE, '');
 
-                code += str.replace(/\\/g, '\\\\').replace(/\r/g, '\\r').replace(/`/g, '\\`').replace(/\${/g, '\\${');
+                code += str.replace(TPL_ESC_RE, escapeTpl);
 
             } else { // JS
                 code += compileIncludes(str, filename, options);
@@ -65,29 +64,28 @@ function compilePart(ejs, filename, options) {
         }
 
         switch (token) {
-        case '%>':
-        case '_%>':
-        case '-%>': code +=
-            prev === '<%=' || prev === '<%-' ? '\n)) + `' :
-            prev === '<%' || prev === '<%_' ? '\n_out += `' :
-            prev === '<%#' ? '' : token;
-            open = null;
-            break;
-        case '<%':
-        case '<%_': code += '`;'; break;
-        case '<%=': code += '` + _esc(_str('; break;
-        case '<%-': code += '` + _str(('; break;
-        case '<%%': code += '<%'; break;
-        case '%%>': code += '%>';
+            case '%>':
+            case '_%>':
+            case '-%>': code +=
+                prev === '<%=' || prev === '<%-' ? '\n)) + `' :
+                prev === '<%' || prev === '<%_' ? '\n_out += `' :
+                prev === '<%#' ? '' : token;
+                open = null;
+                break;
+            case '<%':
+            case '<%_': code += '`;'; break;
+            case '<%=': code += '` + _esc(_str('; break;
+            case '<%-': code += '` + _str(('; break;
+            case '<%%': code += '<%'; break;
+            case '%%>': code += '%>';
         }
 
         prev = token;
-        lastIndex = RE.lastIndex;
+        if (match) lastIndex = match.index + token.length;
 
     } while (match);
 
     code += '`; return _out;';
-    RE.lastIndex = originalLastIndex;
 
     return code;
 }
@@ -95,43 +93,27 @@ function compilePart(ejs, filename, options) {
 function compileIncludes(js, filename, options) {
     const {read, resolve, cache, localsName} = options;
     let code = '';
+    let lastIndex = 0;
 
-    const originalLastIndex = INCLUDE_RE.lastIndex;
-    let lastIndex = INCLUDE_RE.lastIndex = 0;
-    let match;
-
-    while ((match = INCLUDE_RE.exec(js)) !== null) {
-        const includePath = match[2];
-        const includeData = match[4];
+    for (const match of js.matchAll(INCLUDE_RE)) {
+        const [, , includePath, , includeData] = match;
         if (!read) throw new Error(`Found an include but read option missing: ${includePath}`);
 
-        const before = js.slice(lastIndex, match.index);
         const key = resolve(filename, includePath);
-        const includeCode = cache[key] = cache[key] || compilePart(read(key), key, options);
+        cache[key] ??= compilePart(read(key), key, options);
         const includeLocals = includeData ? `Object.assign(Object.create(${localsName}), ${includeData})` : '';
 
-        code += `${before}((${includeLocals ? localsName : ''}) => { ${includeCode} })(${includeLocals})`;
-
-        lastIndex = INCLUDE_RE.lastIndex;
+        code += `${js.slice(lastIndex, match.index)}((${includeLocals ? localsName : ''}) => { ${cache[key]} })(${includeLocals})`;
+        lastIndex = match.index + match[0].length;
     }
 
-    code += js.slice(lastIndex);
-    INCLUDE_RE.lastIndex = originalLastIndex;
-
-    return code;
+    return code + js.slice(lastIndex);
 }
 
 function stringify(v) {
-    return v === null || v === undefined ? '' : String(v);
+    return v == null ? '' : String(v);
 }
 
-const escapeChar = c => (
-    c === '&' ? '&amp;' :
-    c === '<' ? '&lt;' :
-    c === '>' ? '&gt;' :
-    c === '\'' ? '&apos;' :
-    c === '"' ? '&quot;' : c);
-
 function escapeXML(xml) {
-    return xml && xml.replace(ESCAPE_RE, escapeChar);
+    return xml.replace(ESCAPE_RE, escapeChar);
 }
